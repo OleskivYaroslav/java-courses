f34e8a5e5b66b0fbbb21d991c5e3719b31a9f57b
OleskivYaroslav <slavko.ol96@mail.ru>
2016-04-14 02:29:33 +0300

    Full Calculate

--- Calculate.java
+++ Calculate.java
@@ -1,8 +1,29 @@
 public class Calculate{
 
 	public static void main(String[] arg){
-
 		System.out.println("Calculate...");
+		double first = Double.valueOf(arg[0]);
+		double second = Double.valueOf(arg[2]);
+		double sum=0;
+		switch (arg[1]) {
+			case ("+"):
+				sum = first + second;
+				break;
+			case ("-"):
+				sum = first - second;
+				break;
+			case ("*"):
+				sum = first * second;
+				break;
+			case ("/"):
+				sum = (Double)first / second;
+				break;
+			case ("^"):
+				sum = Math.pow(first, second);
+				break;
+			default: System.out.println("Посчитать не удалось");
+		}
+		System.out.println("Sum = "+sum);
 	}
 
 }
