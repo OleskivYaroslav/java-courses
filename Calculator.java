25192b9bb82930ff07b888ffb8831f18c9769af5
OleskivYaroslav <slavko.ol96@mail.ru>
2016-04-18 15:56:41 +0300

    My calculator =)

--- Calculator.java
+++ Calculator.java
@@ -7,6 +7,36 @@ public class Calculator{
         }
     }
 
+    public void minus(double param1,double param2){
+        this.result=param1-param2;
+    }
+
+    public void devide(double param1,double param2){
+        this.result=param1/param2;
+    }
+
+    public void product(double ... params){
+        for (Double param : params){
+            this.result*=param;
+        }
+    }
+
+    public void addToResult(double param){
+        this.result+=param;
+    }
+
+    public void minusOfResult(double param){
+        this.result-=param;
+    }
+
+    public void devideRusult(double param){
+        this.result/=param;
+    }
+
+    public void productOfResult(double param){
+        this.result*=param;
+    }
+
     public double getResult(){
         return this.result;
     }
--- InteractRuner.java
+++ InteractRuner.java
@@ -8,14 +8,78 @@ public class InteractRuner{
         try{
             Calculator calc = new Calculator();
             String exit = "no";
+            System.out.println("Enter problem ");
+            String first = reader.next();
+            String sign = reader.next();
+            String second = reader.next();
+            switch (sign) {
+                case ("+"):
+                    calc.add(Double.valueOf(first), Double.valueOf(second));
+                    break;
+                case ("-"):
+                    calc.minus(Double.valueOf(first), Double.valueOf(second));
+                    break;
+                case ("*"):
+                    calc.product(Double.valueOf(first), Double.valueOf(second));
+                    break;
+                case ("/"):
+                    calc.devide(Double.valueOf(first), Double.valueOf(second));
+                    break;
+                default:
+                    System.out.println("Some ERROR ");
+            }
+            System.out.println("Result = "+ calc.getResult());
+            System.out.println("Exit : yes/no");
+            exit = reader.next();
+
             while (!exit.equals("yes")){
-                System.out.println("Enter first arg : ");
-                String first = reader.next();
-                System.out.println("Enter second arg : ");
-                String second = reader.next();
-                calc.add(Double.valueOf(first),Double.valueOf(second));
+                System.out.println("Used previos result? : yes/no");
+                String res = reader.next();
+                if(res.equals("yes")){
+                    System.out.println("Enter problem ");
+                    System.out.print("result ");
+                    sign = reader.next();
+                    first = reader.next();
+                    switch (sign) {
+                        case ("+"):
+                            calc.addToResult(Double.valueOf(first));
+                            break;
+                        case ("-"):
+                            calc.minusOfResult(Double.valueOf(first));
+                            break;
+                        case ("*"):
+                            calc.productOfResult(Double.valueOf(first));
+                            break;
+                        case ("/"):
+                            calc.devideRusult(Double.valueOf(first));
+                            break;
+                        default:
+                            System.out.println("Some ERROR");
+                    }
+                }else{
+                    calc.cleanResult();
+                    System.out.println("Enter problem ");
+                    first = reader.next();
+                    sign = reader.next();
+                    second = reader.next();
+                    switch (sign) {
+                        case ("+"):
+                            calc.add(Double.valueOf(first), Double.valueOf(second));
+                            break;
+                        case ("-"):
+                            calc.minus(Double.valueOf(first), Double.valueOf(second));
+                            break;
+                        case ("*"):
+                            calc.product(Double.valueOf(first), Double.valueOf(second));
+                            break;
+                        case ("/"):
+                            calc.devide(Double.valueOf(first), Double.valueOf(second));
+                            break;
+                        default:
+                            System.out.println("Some ERROR");
+                    }
+                }
                 System.out.println("Result = "+ calc.getResult());
-                calc.cleanResult();
                 System.out.println("Exit : yes/no");
                 exit = reader.next();
             }
